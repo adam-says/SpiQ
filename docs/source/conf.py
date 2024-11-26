2c20dbb8a738d0d7257c7288a5d09795e291e923
Michele Giugliano <9309421+mgiugliano@users.noreply.github.com>
2024-11-26 11:22:46 +0100

    add readthedocs support

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,10 +13,10 @@ author = 'Michele GIUGLIANO'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.autosectionlabel', "sphinx_rtd_theme"]
 
 templates_path = ['_templates']
-exclude_patterns = []
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 
